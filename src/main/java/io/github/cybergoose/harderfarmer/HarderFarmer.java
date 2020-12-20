6354d296f91c1846d5901965217c651e7a567acf
BuildTools <cybergoose13@gmail.com>
2020-12-19 17:02:00 -0800

    first remote commit

--- src/main/java/io/github/cybergoose/harderfarmer/HarderFarmer.java
+++ src/main/java/io/github/cybergoose/harderfarmer/HarderFarmer.java
@@ -1,5 +1,6 @@
 package io.github.cybergoose.harderfarmer;
 
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class HarderFarmer extends JavaPlugin {
@@ -7,7 +8,7 @@ public final class HarderFarmer extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        this.getServer().getConsoleSender().sendMessage(ChatColor.GREEN + this.getServer().getName() + " is enabled.");
     }
 
     @Override
