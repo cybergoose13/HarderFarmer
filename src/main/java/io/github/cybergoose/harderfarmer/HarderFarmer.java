cc12cd289ac4d685562f2f31160218ba51bb70f6
BuildTools <cybergoose13@gmail.com>
2020-12-30 18:32:10 -0800

    added database support

--- src/main/java/io/github/cybergoose/harderfarmer/HarderFarmer.java
+++ src/main/java/io/github/cybergoose/harderfarmer/HarderFarmer.java
@@ -14,8 +14,15 @@ import io.github.cybergoose.harderfarmer.handlers.FarmingDrop;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public final class HarderFarmer extends JavaPlugin {
 
+    private Connection connection;
+    private String host, database, username, password;
+    private int port;
     @Override
     public void onEnable() {
         loadConfig();
@@ -28,12 +35,30 @@ public final class HarderFarmer extends JavaPlugin {
         this.getServer().getConsoleSender().sendMessage(ChatColor.RED + this.getServer().getName() + " is disabled.");
     }
 
-    public void loadConfig(){
-        this.getConfig().options().copyDefaults(true);
+    private void loadConfig(){
 
-//        this.getConfig().set("HECTOR", "this is a hector");
-//        ConfigManager configManager= new ConfigManager(this, "test");
-//        configManager.addComment("Its", "Me");
+        this.getConfig().options().copyDefaults(true);
+        this.host= this.getConfig().getString("database.host");
+        this.database= this.getConfig().getString("database.dbname");
+        this.username= this.getConfig().getString("database.user");
+        this.password= this.getConfig().getString("database.pswd");
+        this.port= this.getConfig().getInt("database.port");
+
+        try{
+            dbconnect();
+        }catch (SQLException exception){
+            exception.printStackTrace();
+        }
         this.saveConfig();
+    }
+
+    private void dbconnect() throws SQLException {
+        if(connection != null && !connection.isClosed()){
+            return;
+        }
+        connection = DriverManager.getConnection("jdbc:mysql://" + this.host + ":" +
+                this.port + "/" + this.database,
+                this.username, this.password);
+        this.getServer().getConsoleSender().sendMessage("Connection Successful");
     }
 }
---
+++ src/main/java/io/github/cybergoose/harderfarmer/handlers/PlayerJoin.java
@@ -0,0 +1,13 @@
+package io.github.cybergoose.harderfarmer.handlers;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class PlayerJoin implements Listener {
+
+    @EventHandler
+    public void PlayerJoin(PlayerJoinEvent event){
+
+    }
+}
